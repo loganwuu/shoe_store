7c5ab4e0a999ced593ead0e1cc6376dc2b21a549
Logan <student@epicodus.com>
2015-08-28 09:54:18 -0700

    add Brand.php

---
+++ README.md
@@ -0,0 +1,53 @@
+# Shoe Stores with MySQL
+
+##### The application allows users to search a list of local shoe stores and the brands of shoes they carry. Please see [library_database_design.png](library_database_design.png) for the database design.
+(8/28/15)
+
+#### By Logan Wu
+
+## Description
+
+This application allows salon users to add shoe stores and brands and connect between them. The ```stores```, ```brands``` and ```brands_stores``` tables are stored in the ```shoes``` MySQL database.
+
+## Setup
+
+* Clone the project using the link provided on Github.
+* Run composer install in Terminal from the project root folder.
+* Start the PHP server from Terminal in the /web folder.
+* Open a web browser and navigate to "localhost:8000".
+
+### How to Setup Database on MySQL
+
+* ```CREATE DATABASE shoes;```
+* ```USE shoes;```
+* ```CREATE TABLE stores (name VARCHAR(255), id serial PRIMARY KEY);```
+* ```CREATE TABLE brands (name VARCHAR(255), id serial PRIMARY KEY);```
+* ```CREATE TABLE brands_stores (brand_id INT, store_id INT, id serial PRIMARY KEY);```
+
+## Technologies Used
+
+PHP, PHPUnit, Silex, Twig, and MySQL
+
+### Legal
+
+Copyright (c) 2015 **Logan Wu**
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ src/Brand.php
@@ -0,0 +1,89 @@
+<?php
+    class Brand
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (name) VALUES ('{$this->getName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+            $brands = array();
+            foreach($returned_brands as $brand) {
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM brands;");
+        }
+
+        function addStore($store)
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands_stores (brand_id, store_id) VALUES ({$this->getId()}, {$store->getId()});");
+        }
+
+        function getStores()
+        {
+        $query = $GLOBALS['DB']->query("SELECT stores.* FROM brands
+            JOIN brands_stores ON (brands.id = brands_stores.brand_id)
+            JOIN stores on ( brands_stores.store_id = stores.id)
+            WHERE brands.id = {$this->getId()};");
+        $stores = $query->fetchAll(PDO::FETCH_ASSOC);
+        $stores_array = array();
+
+        foreach($stores as $store) {
+            $name = $store['name'];
+            $address = $store['address'];
+            $id = $store['id'];
+            $new_store = new Store($name, $address, $id);
+            array_push($stores_array, $new_store);
+        }
+            return $stores;
+        }
+
+        static function find($search_id)
+        {
+            $found_brand = null;
+            $brands = brand::getAll();
+            foreach($brands as $brand) {
+                $brand_id = $brand->getId();
+                if ($brand_id == $search_id) {
+                    $found_brand = $brand;
+                }
+            }
+            return $found_brand;
+        }
+    }
+?>
